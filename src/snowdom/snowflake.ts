1205d8c956e27bd205a83f020174145ebe016ae2
Oliver Schürch <oliver.schuerch@bambit.ch>
2025-12-07 00:31:28 +0100

    refactor: improve snowflake

--- src/snowdom/snowflake.ts
+++ src/snowdom/snowflake.ts
@@ -11,12 +11,12 @@ export class SnowDOMFlake {
   stageBox: DOMRect;
 
   private randoms: Randoms = {
-    fillStyle: Math.max(0.2, Math.min(0.8, Math.random())), // min 0.2, max 0.8
+    fillStyle: Math.random() * 0.6 + 0.2, // min 0.2, max 0.8
     x: Math.random() * 1.3 - 0.15, // min -0.15, max 1.15
     y: Math.random(),
-    size: Math.max(2, Math.random() * 3), // min 2, max 3
-    velocityX: Math.min(0.8, Math.random()), // min 0, max 0.8
-    velocityY: Math.max(0.5, Math.random()), // min 0.5, max 1
+    size: Math.random() * 2 + 3, // min 3, max 5
+    velocityX: Math.random() * 0.8, // min 0, max 0.8
+    velocityY: Math.random() * 0.5 + 0.5, // min 0.5, max 1
   };
 
   private fillStyle: string = `rgba(255, 255, 255, ${this.randoms.fillStyle})`;
@@ -83,84 +83,3 @@ export class SnowDOMFlake {
     this.offsetY = (this.randoms.y * this.stageBox.height + this.size) * -1;
   }
 }
-
-/*
-initFlakes(box: DOMRect) {
-  this.flakes = [];
-  for (var i = 0; i < this.options.flakeCount; i++) {
-    const x = Math.floor(Math.random() * box.width);
-    const y = Math.floor(Math.random() * box.height);
-    const size = Math.random() * 3 + 2;
-    const speed = Math.random() * 1 + 0.5;
-    const opacity = Math.random() * 0.5 + 0.3;
-    this.flakes.push({
-      speed: speed,
-      velY: speed,
-      velX: 0,
-      x: x,
-      y: y,
-      size: size,
-      stepSize: Math.random() / 30,
-      step: 0,
-      angle: 180,
-      opacity: opacity,
-    });
-  }
-}
-*/
-
-// renderSnowflakes() {
-//   this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-//   for (var i = 0; i < this.options.flakeCount; i++) {
-//     const flake = this.flakes[i];
-//     const minDist = 150;
-//     const x2 = flake.x;
-//     const y2 = flake.y;
-//     const dist = Math.sqrt(x2 * x2 + y2 * y2);
-
-//     if (dist < minDist) {
-//       const force = minDist / (dist * dist);
-//       const xcomp = x2 / dist;
-//       const ycomp = y2 / dist;
-//       const deltaV = force / 2;
-
-//       flake.velX -= deltaV * xcomp;
-//       flake.velY -= deltaV * ycomp;
-//     } else {
-//       flake.velX *= 0.98;
-//       if (flake.velY <= flake.speed) {
-//         flake.velY = flake.speed;
-//       }
-//       flake.velX += Math.cos((flake.step += 0.05)) * flake.stepSize;
-//     }
-
-//     this.context.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
-//     flake.y += flake.velY;
-//     flake.x += flake.velX;
-
-//     if (flake.y >= this.canvas.height || flake.y <= 0) {
-//       this.resetSnowflake(flake);
-//     }
-
-//     if (flake.x >= this.canvas.width || flake.x <= 0) {
-//       this.resetSnowflake(flake);
-//     }
-
-//     this.context.beginPath();
-//     this.context.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
-//     this.context.fill();
-//   }
-// }
-
-/*
-resetSnowflake(flake: any) {
-  flake.x = Math.floor(Math.random() * this.canvas.width);
-  flake.y = 0;
-  flake.size = Math.random() * 3 + 2;
-  flake.speed = Math.random() * 1 + 0.5;
-  flake.velY = flake.speed;
-  flake.velX = 0;
-  flake.opacity = Math.random() * 0.5 + 0.3;
-}
-*/
